49fc7ae3824ee6de14dff4faff6332050cd56310
Bryan Bergen <bryanbergen@gmail.com>
2015-07-24 22:28:21 -0700

    initial design for brick breaker

--- breaker.c
+++ breaker.c
@@ -1,12 +1,60 @@
 //
 // Created by bryan on 7/24/15.
 //
-#include <SDL2/SDL.h>
 #include "breaker.h"
 #include "common.h"
 
+static void
+render_brick(SDL_Renderer *renderer, breaker_brick *brick) {
+    //TODO implement
+}
+
+static void
+render_ball(SDL_Renderer *renderer, breaker_ball *ball) {
+    //TODO implement
+}
+
+static void
+render_paddle(SDL_Renderer *renderer, breaker_paddle *paddle) {
+    //TODO implement
+}
+
+static void
+render(SDL_Renderer *renderer, breaker_game *game) {
+    //TODO implement
+}
+
+static void
+update(breaker_game *game) {
+    //TODO implement
+}
+
+static void
+error(const char*(*error_function)(void)) {
+    fprintf(stderr, "Error: %s\n", error_function());
+    exit(EXIT_FAILURE);
+}
+
+static void
+run(void) {
+    //TODO implement resource init, game state, game loop
+    int8_t running = true;
+}
+
+static void
+init(void) {
+    //TODO implement SDL Lib initializations, error checks
+}
+
+static void
+close(void) {
+    SDL_Quit();
+}
+
 int
 main(int argc, char** argv) {
-    fprintf(stdout, "Brick Breaker Ready To Go!\n");
+    init();
+    run();
+    close();
     return EXIT_SUCCESS;
 }
--- breaker.h
+++ breaker.h
@@ -5,4 +5,44 @@
 #ifndef SDL_GRAPHICS_BREAKER_H
 #define SDL_GRAPHICS_BREAKER_H
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+typedef struct breaker_sounds {
+    Mix_Music *music;
+    Mix_Chunk *paddle_bounce;
+    Mix_Chunk *brick_break;
+};
+
+typedef struct breaker_brick {
+    int x;
+    int y;
+    int width;
+    int height;
+    int point_value;
+    SDL_Color *color;
+} breaker_brick;
+
+typedef struct breaker_paddle {
+    float x;
+    float y;
+    int width;
+    int height;
+    SDL_Color *color;
+} breaker_paddle;
+
+typedef struct breaker_ball {
+    float x;
+    float y;
+    int radius;
+    SDL_Color *color;
+} breaker_ball;
+
+typedef struct breaker_game {
+    breaker_paddle *player;
+    breaker_ball *ball;
+    float difficulty_modifier;
+    uint32_t score;
+} breaker_game;
+
 #endif //SDL_GRAPHICS_BREAKER_H
